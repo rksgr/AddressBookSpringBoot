57dae6ad90022df483aa8cf184c0b007fc3d7fd5
rksgr <r_kjha@yahoo.com>
2022-02-09 06:23:05 +0530

    Added: DTO for addressbook

--- src/main/java/com/example/springaddressbook/DTO/AddressBookDTO.java
+++ src/main/java/com/example/springaddressbook/DTO/AddressBookDTO.java
@@ -1,2 +1,19 @@
-package com.example.springaddressbook.DTO;public class AddressBookDTO {
+package com.example.springaddressbook.DTO;
+
+public class AddressBookDTO {
+    public String name;
+    public String city;
+
+    public AddressBookDTO(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                '}';
+    }
 }
