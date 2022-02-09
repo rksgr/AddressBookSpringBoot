47858d364d4eb991a321f220a937bfd5ae9affa9
rksgr <r_kjha@yahoo.com>
2022-02-09 06:47:59 +0530

    Refactored: Use lombok library for logging

--- src/main/java/com/example/springaddressbook/SpringAddressBookApplication.java
+++ src/main/java/com/example/springaddressbook/SpringAddressBookApplication.java
@@ -1,13 +1,16 @@
 package com.example.springaddressbook;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class SpringAddressBookApplication {
 
+    /**UC 2: Use Lombok library for logging */
     public static void main(String[] args) {
         SpringApplication.run(SpringAddressBookApplication.class, args);
+        log.info("Address Book App started.");
     }
-
 }
