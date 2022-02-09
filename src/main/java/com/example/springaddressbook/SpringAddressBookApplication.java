f6660f573883c48388c292dac94115f03bf2f3c3
rksgr <r_kjha@yahoo.com>
2022-02-09 07:40:54 +0530

    Refactored: Determine the logging levels, logging to console or file logging patterns etc based on this application running in development, staging or production mode

--- src/main/java/com/example/springaddressbook/SpringAddressBookApplication.java
+++ src/main/java/com/example/springaddressbook/SpringAddressBookApplication.java
@@ -3,14 +3,18 @@ package com.example.springaddressbook;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class SpringAddressBookApplication {
 
-    /**UC 2: Use Lombok library for logging */
+    /**UC3 : Determine the logging levels, logging to console or file logging patterns etc based on this application running
+     * in Development, staging or production **/
     public static void main(String[] args) {
-        SpringApplication.run(SpringAddressBookApplication.class, args);
-        log.info("Address Book App started.");
+        ApplicationContext context = SpringApplication.run(SpringAddressBookApplication.class, args);
+
+        // Logging the environment from appropriate application property
+        log.info("Employee Payroll App started in {} environment",context.getEnvironment().getProperty("environment"));
     }
 }
