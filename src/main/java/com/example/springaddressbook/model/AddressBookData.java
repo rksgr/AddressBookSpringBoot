57f72500f8dc0c5dba72a7163a2b033c315b83b8
rksgr <r_kjha@yahoo.com>
2022-02-13 22:52:43 +0530

    Added: Model class

--- src/main/java/com/example/springaddressbook/model/AddressBookData.java
+++ src/main/java/com/example/springaddressbook/model/AddressBookData.java
@@ -1,2 +1,27 @@
-package com.example.springaddressbook.model;public class AddressBookData {
+package com.example.springaddressbook.model;
+
+public class AddressBookData {
+    private String name;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public AddressBookData(String name, String city){
+        this.name = name;
+        this.city = city;
+    }
 }
