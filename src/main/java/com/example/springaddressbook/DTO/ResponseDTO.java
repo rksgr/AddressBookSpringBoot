a6346d612f83e434551f618c8df9aeabb79b36e8
rksgr <r_kjha@yahoo.com>
2022-02-13 22:39:48 +0530

    Added: Response DTO class

--- src/main/java/com/example/springaddressbook/DTO/ResponseDTO.java
+++ src/main/java/com/example/springaddressbook/DTO/ResponseDTO.java
@@ -1,2 +1,23 @@
-package com.example.springaddressbook.DTO;public class ResponseDTO {
+package com.example.springaddressbook.DTO;
+
+public class ResponseDTO {
+    private String message;
+    private Object data;
+
+    public String getMessage(){
+        return message;
+    }
+    public void setMessage(){
+        this.message = message;
+    }
+    public Object getData(){
+        return data;
+    }
+    public void setData(){
+        this.data = data;
+    }
+    public ResponseDTO(String message, Object data){
+        this.message = message;
+        this.data = data;
+    }
 }
