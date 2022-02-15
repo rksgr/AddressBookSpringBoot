03e59fd2fd8725430186af3ba75edbc888d455ca
rksgr <r_kjha@yahoo.com>
2022-02-15 10:54:05 +0530

    Refactored: Added new fields address, state, zip, phone and email

--- src/main/java/com/example/springaddressbook/DTO/AddressBookDTO.java
+++ src/main/java/com/example/springaddressbook/DTO/AddressBookDTO.java
@@ -1,23 +1,25 @@
 package com.example.springaddressbook.DTO;
 
 import lombok.Data;
+import lombok.ToString;
 
-/** UC 1: Use lombok library to auto generate getters and setters for the DTO */
-public @Data class AddressBookDTO {
-
+/** UC 1: Use lombok library to auto generate getters and setters and override toString() method for the DTO */
+public @Data @ToString class AddressBookDTO {
     public String name;
+    private String address;
     public String city;
+    public String state;
+    public int zip;
+    private String emailId;
+    private float phoneNo;
 
-    public AddressBookDTO(String name, String city) {
+    public AddressBookDTO(String name,String address, String city, String state,int zip,float phoneNo, String emailId) {
         this.name = name;
+        this.address = address;
         this.city = city;
-    }
-
-    @Override
-    public String toString() {
-        return "{" +
-                "name='" + name + '\'' +
-                ", city='" + city + '\'' +
-                '}';
+        this.state = state;
+        this.zip = zip;
+        this.emailId = emailId;
+        this.phoneNo = phoneNo;
     }
 }
