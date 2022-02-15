ac1f09b1f226b064485ded5ffd62680f6dd38a73
rksgr <r_kjha@yahoo.com>
2022-02-15 10:58:07 +0530

    Refactored: Added new fields address, state, zip, phone and email and changed constructor parameters accordingly

--- src/main/java/com/example/springaddressbook/model/AddressBookData.java
+++ src/main/java/com/example/springaddressbook/model/AddressBookData.java
@@ -1,33 +1,27 @@
 package com.example.springaddressbook.model;
 
 import com.example.springaddressbook.DTO.AddressBookDTO;
+import lombok.Data;
 
-public class AddressBookData {
+public @Data class AddressBookData {
+
     private String name;
+    private String address;
     private String city;
+    private String state;
+    private int zip;
+    private float phoneNo;
+    private String emailId;
 
-    public String getName() {
-        return name;
-    }
+    public AddressBookData(){}
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public AddressBookData(String name, String city){
-        this.name = name;
-        this.city = city;
-    }
     public AddressBookData(AddressBookDTO addressBookDTO){
         this.name = addressBookDTO.getName();
+        this.address = addressBookDTO.getAddress();
         this.city = addressBookDTO.getCity();
+        this.state = addressBookDTO.getState();
+        this.zip = addressBookDTO.getZip();
+        this.phoneNo = addressBookDTO.getPhoneNo();
+        this.emailId = addressBookDTO.getEmailId();
     }
 }
