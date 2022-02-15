3914d885ef91959f409e4e23010433a35d978200
rksgr <r_kjha@yahoo.com>
2022-02-15 08:39:57 +0530

    Added: Service interface containing methods to be implemented by the service class

--- src/main/java/com/example/springaddressbook/service/IAddressBookService.java
+++ src/main/java/com/example/springaddressbook/service/IAddressBookService.java
@@ -1,2 +1,19 @@
-package com.example.springaddressbook.service;public interface IAddressBookService {
+package com.example.springaddressbook.service;
+
+import com.example.springaddressbook.DTO.AddressBookDTO;
+import com.example.springaddressbook.model.AddressBookData;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
+public interface IAddressBookService{
+
+    List<AddressBookData> getAddressBookData();
+
+    AddressBookData createAddressBookData(AddressBookDTO addressBookDTO);
+
+    AddressBookData updateAddressBookData(AddressBookDTO addressBookDTO, String name) throws
+            IllegalAccessException, NoSuchMethodException, InvocationTargetException;
+
+    boolean deleteAddressBookData(String name);
 }
